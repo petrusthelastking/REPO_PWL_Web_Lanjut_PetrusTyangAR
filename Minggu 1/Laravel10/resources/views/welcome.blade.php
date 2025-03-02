2007585492840d72d161c0413fd1bd4d73182c88
petrusthelastking <143620112+petrusthelastking@users.noreply.github.com>
2025-03-03 01:38:09 +0700

    feat : jobsheet 1

---
+++ Minggu 1/Laravel10/resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="ulf-8">
+        <meta name="viewport" content="width=device width, initial-scale=1">
+        <title>PWL 2024</title>
+    </head>
+    <body>
+        <h2>Pemrograman Web Lanjut 2024</h2>
+        <h2>Semangat Belajar Laravel 10</h2>
+    </body>
+</html>
