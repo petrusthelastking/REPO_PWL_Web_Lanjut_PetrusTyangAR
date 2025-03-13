2567f038e92dd6133f8891beb3a7d9645d0b3dff
petrusthelastking <143620112+petrusthelastking@users.noreply.github.com>
2025-03-13 11:22:19 +0700

    Jobsheet4

--- Minggu4/PWL_POS/app/Http/Controllers/UserController.php
+++ Minggu4/PWL_POS/app/Http/Controllers/UserController.php
@@ -120,23 +120,68 @@ class UserController extends Controller
             // $user->isClean(); // true
             // dd($user->isDirty());
         
-        $user = UserModel::create([
-                'username' => 'manager11',
-                'nama' => 'Manager11',
-                'password' => Hash::make('12345'),
-                'level_id' => 2,
+        // $user = UserModel::create([
+        //         'username' => 'manager11',
+        //         'nama' => 'Manager11',
+        //         'password' => Hash::make('12345'),
+        //         'level_id' => 2,
+        //     ]);
+
+        //     $user->username = 'manager12';
+
+        //     $user->save();
+
+        //     // //wasChanged() digunakan untuk mengecek apakah perubahan benar-benar disimpan ke database setelah save().
+
+        //     $user->wasChanged(); // true
+        //     $user->wasChanged('username'); // true
+        //     $user->wasChanged(['username', 'level_id']); // true
+        //     $user->wasChanged('nama'); // false
+        //     dd($user->wasChanged(['nama', 'username'])); // true
+        //---------------------------------------------------------Praktikum 2.6--------------------------------------------------
+        $user = UserModel::all();
+            return view('user', ['data' => $user]);
+        }
+        public function tambah(){
+            return view('user_tambah');
+        }
+
+        public function tambah_simpan(Request $request) //Fungsi ini menerima request dari form yang dikirim oleh pengguna.
+        {
+            UserModel::create([
+                'username' => $request->username,
+                'nama' => $request->nama,
+                'password' => Hash::make('$request->password'),
+                'level_id' => $request->level_id
             ]);
 
-            $user->username = 'manager12';
+            return redirect('/user');
+        }
+
+        public function ubah($id){
+            $user = UserModel::find($id);
+            return view('user_ubah', ['data' => $user]);
+        }
+
+        public function ubah_simpan($id, Request $request)
+        {
+            $user = UserModel::find($id);
+
+            $user->username = $request->username;
+            $user->nama = $request->nama;
+            $user->password = Hash::make('$request->password');
+            $user->level_id = $request->level_id;
 
             $user->save();
 
-            // //wasChanged() digunakan untuk mengecek apakah perubahan benar-benar disimpan ke database setelah save().
+            return redirect('/user');
+        }
 
-            $user->wasChanged(); // true
-            $user->wasChanged('username'); // true
-            $user->wasChanged(['username', 'level_id']); // true
-            $user->wasChanged('nama'); // false
-            dd($user->wasChanged(['nama', 'username'])); // true
-    }
+        public function hapus($id)
+        {
+            $user = UserModel::find($id); //Mengambil semua data dari tabel m_user, sekaligus mengambil data dari tabel m_level berdasarkan relasi level().
+            $user->delete();
+
+            return redirect('/user');
+        }
 }
