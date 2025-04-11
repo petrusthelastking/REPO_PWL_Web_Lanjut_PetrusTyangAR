1edd57f3f985d1f7f74f937a40a180b0d8291d6a
petrusthelastking <143620112+petrusthelastking@users.noreply.github.com>
2025-04-11 22:18:23 +0700

    POS

--- POS/app/Http/Controllers/UserController.php
+++ POS/app/Http/Controllers/UserController.php
@@ -1,15 +1,20 @@
 <?php
 
 namespace App\Http\Controllers;
+
+//memanggil UserModel
 use App\Models\UserModel;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+use function Laravel\Prompts\password;
 
 class UserController extends Controller
 {
-    public function index()
-    {   
-       //----------------------------------------------------Jobsheet 3 - Migration--------------------------------------------------------------------
+    public function index(){
+
+
+            //----------------------------------------------------Jobsheet 3 - Migration--------------------------------------------------------------------
 
             //Menambahkan data baru menggunakan Eloquent
             // $data = [
@@ -31,12 +36,9 @@ class UserController extends Controller
             // //Mengambil semua data menggunakan Eloquent
             // $user = UserModel::all();
             // return view('user', ['data' => $user]);
-        
-        //----------------------------------------------------Jobsheet 4 - Eloquent ORM----------------------------------------------------
 
-            
             //----------------------------------------------------Jobsheet 4 - Eloquent ORM--------------------------------------------------
-            //Praktikum 1
+            //-------------------------------------------------Praktikum 1------------------------------------------------------
             //Menambahkan kode untuk menambahkan data baru
             // $data = [
             //     'level_id' => 2,
@@ -57,8 +59,174 @@ class UserController extends Controller
             // $user = UserModel::all();
             // return view('user', ['data' => $user]);
 
-            //Praktikum 2
-            $user = UserModel::find(1); //Mencari data pengguna dengan primary key ID = 1 di dalam database menggunakan Eloquent ORM
-            return view('user', ['data' => $user]); //Mengembalikan tampilan user.blade.php dengan data pengguna yang ditemukan
+            //-------------------------------------------------Praktikum 2.1------------------------------------------------------
+
+            // $user = UserModel::find(1); //Mencari data pengguna dengan primary key ID = 1 di dalam database menggunakan Eloquent ORM
+
+            // $user = UserModel::where('level_id', 1)->first(); //Mengambil satu data pertama dari tabel yang memiliki level_id = 1 menggunakan Eloquent
+
+            // $user = UserModel::firstWhere('level_id', 1); //shortcut dari where()->first()
+
+            // $user = UserModel::findOr(1, ['username', 'nama'], function(){ //Mencari data pengguna dengan primary key tetapi dengan fallback (alternatif) jika data tidak ditemukan
+            //     //Jika data ditemukan, hanya kolom username dan nama yang akan diambil.
+            //     abort(404); //Jika data tidak ditemukan, maka akan menampilkan error 404
+            // });
+
+            // $user = UserModel::findOr(20, ['username', 'nama'], function () {
+            //     abort(404);
+            // });
+
+            //---------------------------------------------------Praktikum 2.2-------------------------------------------------------
+
+            // $user = UserModel::findOrFail(1); //Mencari data pengguna dengan primary key ID = 1 di dalam database, jika data tidak ditemukan maka akan menampilkan error 404 tanpa perlu menulis abort(404).
+
+            // $user = UserModel::where('username', 'manager9')->firstOrFail(); //Mencari satu data pertama dalam database berdasarkan kriteria username = manager9, jika data tidak ditemukan maka akan menampilkan error 404 (Not Found) tanpa perlu pengecekan manual.
+
+
+            //---------------------------------------------------Praktikum 2.3-------------------------------------------------------
+
+            // $user = UserModel::where('level_id',2)->count(); //Menghitung jumlah data dalam tabel users yang memiliki level_id = 2
+            //Catatan: dd($user); (die and dump) bukan sekadar echo, tetapi metode Laravel untuk debugging. Selain menampilkan nilai variabel, dd() juga menampilkan lokasi kode yang memanggilnya.
+            // dd($user); //Menampilkan hasilnya dengan dd()
+
+            //---------------------------------------------------Praktikum 2.4-------------------------------------------------------
+            //Mencari satu data pertama yang cocok berdasarkan kondisi
+            //Jika data ditemukan, Laravel mengembalikan data yang ada, Jika tidak ditemukan, Laravel akan menyimpan data baru ke database, lalu mengembalikan data tersebut.
+            // $user = UserModel::firstOrCreate(
+            //     [
+            //         'username' => 'manager',
+            //         'nama' => 'Manager'
+            //     ]
+            // );
+
+            // $user = UserModel::firstOrCreate(//Akan melakukan insert data, karena data pada kode ini belum ada di database
+            //     [
+            //         'username' => 'manager22',
+            //         'nama' => 'Manager Dua Dua',
+            //         'password' => Hash::make('12345'),
+            //         'level_id' => 2
+            //     ]
+            // );
+
+            // $user = UserModel::firstOrNew( //Digunakan untuk mencari data pertama berdasarkan kondisi yang diberikan. Jika data ditemukan, Laravel mengembalikan object model yang sudah ada.
+            //                                //Namun, jika data tidak ditemukan, Laravel akan membuat object model baru tetapi tidak langsung menyimpannya ke database. harus memanggil $model->save(); secara manual jika ingin menyimpannya.
+            //     [
+            //         'username' => 'manager',
+            //         'nama' => 'Manager'
+            //     ]
+            // );
+
+            // $user = UserModel::firstOrNew(
+            //     [
+            //         'username' => 'manager33',
+            //         'nama' => 'Manager Tiga Tiga',
+            //         'password' => Hash::make('12345'),
+            //         'level_id' => 2
+            //     ]
+            // );
+            // $user->save(); //Menyimpan data ke database
+            //---------------------------------------------------Praktikum 2.5-------------------------------------------------------
+            // $user = UserModel::create([
+            //     'username' => 'manager55',
+            //     'nama' => 'Manager55',
+            //     'password' => Hash::make('12345'),
+            //     'level_id' => 2,
+            // ]);
+
+            // $user->username = 'manager56'; //variabel $user mengalami perubahan
+
+            // //isDirty() digunakan untuk mengecek apakah model telah diubah sebelum disimpan.
+            // //isClean() digunakan untuk mengecek apakah model masih sama seperti di database (belum diubah).
+
+
+            // $user->isDirty(); // true
+            // $user->isDirty('username'); // true
+            // $user->isDirty('nama'); // false
+            // $user->isDirty(['nama', 'username']); // true
+
+            // $user->isClean(); // false
+            // $user->isClean('username'); // false
+            // $user->isClean('nama'); // true
+            // $user->isClean(['nama', 'username']); // false
+
+            // $user->save(); // data sudah disimpan ke database, sehingga variabel $user diangap bersih atau tidak ada perubahan
+
+            // $user->isDirty(); // false
+            // $user->isClean(); // true
+            // dd($user->isDirty());
+
+
+            // $user = UserModel::create([
+            //     'username' => 'manager11',
+            //     'nama' => 'Manager11',
+            //     'password' => Hash::make('12345'),
+            //     'level_id' => 2,
+            // ]);
+
+            // $user->username = 'manager12';
+
+            // $user->save();
+
+            // //wasChanged() digunakan untuk mengecek apakah perubahan benar-benar disimpan ke database setelah save().
+
+            // $user->wasChanged(); // true
+            // $user->wasChanged('username'); // true
+            // $user->wasChanged(['username', 'level_id']); // true
+            // $user->wasChanged('nama'); // false
+            // dd($user->wasChanged(['nama', 'username'])); // true
+
+            //---------------------------------------------------------Praktikum 2.6--------------------------------------------------
+            // $user = UserModel::all();
+            // return view('user', ['data' => $user]);
+
+            //---------------------------------------------------------Praktikum 2.7--------------------------------------------------
+            $user = UserModel::with('level')->get();
+            // dd($user);
+            return view('user', ['data' => $user]);
+    }
+
+    public function tambah(){
+        return view('user_tambah');
+    }
+
+    public function tambah_simpan(Request $request) //Fungsi ini menerima request dari form yang dikirim oleh pengguna.
+    {
+        UserModel::create([
+            'username' => $request->username,
+            'nama' => $request->nama,
+            // 'password' => Hash::make('$request->password'), //Kesalahan menuliskan '$request->password' sebagai string, seharusnya $request->password. sehingga program tidak akan membaca nilai dari $request password
+            'password' => Hash::make($request->password),
+            'level_id' => $request->level_id
+        ]);
+
+        return redirect('/user');
+    }
+
+    public function ubah($id){
+        $user = UserModel::find($id);
+        return view('user_ubah', ['data' => $user]);
+    }
+
+    public function ubah_simpan($id, Request $request)
+    {
+        $user = UserModel::find($id);
+
+        $user->username = $request->username;
+        $user->nama = $request->nama;
+        // $user->password = Hash::make('$request->password');
+        $user->password = Hash::make($request->password);
+        $user->level_id = $request->level_id;
+
+        $user->save();
+
+        return redirect('/user');
+    }
+
+    public function hapus($id)
+    {
+        $user = UserModel::find($id); //Mengambil semua data dari tabel m_user, sekaligus mengambil data dari tabel m_level berdasarkan relasi level().
+        $user->delete();
+
+        return redirect('/user');
     }
 }
--- POS/routes/web.php
+++ POS/routes/web.php
@@ -29,6 +29,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//--------------------------------------------Jobsheet 3-------------------------------------
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('kategori', [KategoriController::class, 'index']);
-Route::get('user', [UserController::class, 'index']);
+Route::get('/user', [UserController::class, 'index']);
+
+
+//--------------------------------------------Jobsheet 4--------------------------------
+Route::get('/user/tambah', [UserController::class, 'tambah']);
+Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
+Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
+Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
+Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
