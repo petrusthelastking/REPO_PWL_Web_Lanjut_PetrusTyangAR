813cb55e941b3749125a333d7923aaa4979ba850
petrusthelastking <143620112+petrusthelastking@users.noreply.github.com>
2025-04-13 12:03:48 +0700

    POS

--- POS/app/Models/UserModel.php
+++ POS/app/Models/UserModel.php
@@ -21,7 +21,8 @@ class UserModel extends Authenticatable
 
     //=================================================================Jobsheet 4============================================================
     //Menambahkan variabel $fillable untuk mendaftarkan atribut (nama kolom) yang bisa diisi ketika melakukan insert atau update ke database.
-    protected $fillable = ['level_id', 'username', 'nama', 'password'];
+    protected $fillable = ['level_id', 'username', 'nama', 'password' , 'profile_photo'];
+    // protected $guarded = ['user_id']; //Menggunakan variabel $guarded untuk mendaftarkan atribut (nama kolom) yang tidak bisa diisi ketika melakukan insert atau update ke database.
 
     //Menghilangkan kolom password pada variabel $fillable, menandakan variabel password tidak bisa diisi ketika melakukan insert atau update ke database.
     // protected $fillable = ['level_id', 'username', 'nama'];
--- POS/resources/views/level/index.blade.php
+++ POS/resources/views/level/index.blade.php
@@ -6,9 +6,10 @@
     <h3 class="card-title">{{ $page->title }}</h3>
     <div class="card-tools">
         <button onclick="modalAction('{{ url('/level/import') }}')" class="btn btn-info">Import Level</button>
+        {{-- <a class="btn btn-sm btn-primary mt-1" href="{{ url('level/create') }}">Tambah</a> --}}
         <a class="btn btn-primary" href="{{ url('level/export_excel') }}"><i class="fa fa-file-excel"></i>Export Level</a>
-        <a class="btn btn-sm btn-primary mt-1" href="{{ url('level/create') }}">Tambah</a>
-        <button onclick="modalAction('{{ url('/level/create_ajax') }}')" class="btn btn-sm btn-success mt-1">Tambah Ajax</button>
+        <a href="{{ url('/level/export_pdf') }}" class="btn btn-warning"><i class="fa fa-file-pdf"></i> Export Level</a>
+        <button onclick="modalAction('{{ url('/level/create_ajax') }}')" class="btn btn-success">Tambah Ajax</button>
     </div>
     </div>
     <div class="card-body">
@@ -72,6 +73,12 @@ $(document).ready(function() {
         }
     ]
 
+    });
+
+    $('#table_level_filter input').unbind().bind().on('keyup', function(e){
+        if(e.keyCode == 13){ // enter key
+            dataLevel.search(this.value).draw();
+        }
     });
 });
 </script>
