7089b79ce3a00a5afaa0ca3bff3a9f44a55aacf8
petrusthelastking <143620112+petrusthelastking@users.noreply.github.com>
2025-04-24 17:56:45 +0700

    Jobsheet10

--- Minggu10/POS/routes/api.php
+++ Minggu10/POS/routes/api.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\RegisterController;
+use App\Http\Controllers\Api\LogoutController;
+use App\Http\Controllers\Api\ProfileController;
+use App\Http\Controllers\Api\LevelController;
+use App\Http\Controllers\Api\LevelModelController;  
+
 
 /*
 |--------------------------------------------------------------------------
@@ -22,3 +29,11 @@ Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('lo
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
+
+//level
+Route::get('levels', [LevelController::class, 'index']);
+Route::post('levels', [LevelController::class, 'store']);
+Route::get('levels/{level}', [LevelController::class, 'show']);
+Route::put('levels/{level}', [LevelController::class, 'update']);
+Route::delete('levels/{level}', [LevelController::class, 'destroy']);
